37251811dc0002091280b2ea1f84d7011c7d3c17
gopi <gopimathilakath@gmail.com>
2026-05-17 20:41:05 +0530

    fix env variables again

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import multer from 'multer'
 import fs from 'fs'
 import { exec } from 'child_process'
 import fetch from 'node-fetch'
-const API_URL = import.meta.env.VITE_API_URL
+
 
 const app = express()
 app.use(cors())
